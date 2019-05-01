c15584426aa62217c3177f93f7a9ba29169d5582
IsaacGu <crazyvalse@yeah.net>
2019-05-01 10:18:06 +0800

    move dart files into related folders.

---
+++ 02_function/01_isNoble.dart
@@ -0,0 +1,9 @@
+var _nobleGases = {'first': 'partridge'};
+
+bool isNoble(int atomicNumber) {
+  return _nobleGases[atomicNumber] != null;
+}
+
+bool isNoble2(atomicNumber) => _nobleGases[atomicNumber] != null;
+
+
